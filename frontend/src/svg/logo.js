c9be93adcd26981c952ba13d36679961a4df54e4
MOHAMED THOWFICK M <mdthowfick1info@gmail.com>
2023-07-18 13:44:13 +0530

    frontend update

---
+++ frontend/src/svg/logo.js
@@ -0,0 +1,13 @@
+function Logo() {
+    return (
+      <img
+        src="https://bus-admin-web.web.app/static/media/logo.60238d87e338850fe052ec918864cc2e.svg"
+        alt="Logo"
+        width="40"
+        height="40"
+      />
+    );
+  }
+  
+  export default Logo;
+  
